b0f8f63e3fbbe9b01a60c564d9567079b493af7d
rob2fresh4this <grijrob07@gmail.com>
2024-04-04 10:55:40 -0700

    work on page 1

--- src/components/Page1.tsx
+++ src/components/Page1.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Page1.css"
+import logo from "../Images/Placeholder.svg"
+import logo1x1 from "../Images/placeholder1x1.jpg"
+import { Flex } from 'antd'
 
 
 const Page1: React.FC = () => {
     return (
-        <>
-            <h1 className="words">words</h1>
-            <h2>words</h2>
-        </>
+        <Flex justify={"center"} gap="150px" vertical={false}>
+            <Flex vertical={true}>
+            <img src={logo1x1} alt="" width="50%" />
+            <img src={logo1x1} alt="" width="50%" />
+            </Flex>
+        <Flex justify={"center"} vertical={true} >
+            <img className="img1x1" src={logo} alt="" />
+        </Flex>
+        </Flex>
+
+
     )
     
 }
