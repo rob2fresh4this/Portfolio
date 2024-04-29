b42660b6a15d8932d1603d591210ea38fad9ebe4
OlGreyFox <87340574+OlGreyFox@users.noreply.github.com>
2024-04-29 02:14:27 +0000

    to percent

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../index.css';
 import { useState } from 'react';
 import NavBar from './NavBar';
 import MainDisplay from './MainDisplay';
@@ -12,7 +11,7 @@ const Body: React.FC = () => {
     }
     
     return (
-        <div className='font '>
+        <div className='font'>
             <NavBar onClick={handleClick} />
             <MainDisplay displayKey={displayKey}/>
         </div>
